a17f8b9ed13ec112c70a1d614019d2c8fa94f549
Rahul Nadella <rahulnadella@yahoo.com>
2014-10-15 11:29:51 -0400

    Initial Commit

---
+++ XmlReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XmlReader
+//
+//  Created by Rahul Nadella on 10/15/14.
+//  Copyright (c) 2014 Rahul Nadella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
